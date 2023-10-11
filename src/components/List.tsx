c28c5d054c6adf551cd4147cbbd6050bf6aa4033
nogi <Createc369.Re@gmail.com>
2023-10-11 19:57:16 +0900

    add: ListComponent

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,2 +1,18 @@
 // リスト表示
-const TaskList = () => {};
+
+import { FC } from "react";
+import { Todo } from "./Todo";
+
+type TaskListProps = {
+  todos: Todo[];
+};
+
+export const TaskList: FC<TaskListProps> = ({ todos }) => {
+  return (
+    <ul>
+      {todos.map((todo) => {
+        return <li key={todo.id}>{todo.text}</li>;
+      })}
+    </ul>
+  );
+};
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-type Todo = {
+export type Todo = {
   id: number;
   text: string;
 };
